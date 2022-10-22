54f7f76ccdd4c869c8a722b681bc7cdf0623eff5
Hoan Tran <xhoantran@gmail.com>
2022-10-22 00:57:33 -0500

    update config

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,13 +1,14 @@
 import os
 
+import api.routing
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
-import api.routing
-
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
 application = ProtocolTypeRouter(
     {
