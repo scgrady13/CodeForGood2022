cacd2ebc4ce475c6707fb7a65ebeeee2f0e6a34b
Hoan Tran <xhoantran@gmail.com>
2022-10-22 02:51:55 -0500

    update proc

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -4,20 +4,17 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django.urls import path
+from django.urls import re_path
+from api.consumers import HealthDataConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django_asgi_app = get_asgi_application()
 
-from api.consumers import HealthDataConsumer
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(
-                URLRouter[path("health-data/", HealthDataConsumer.as_asgi())]
-            )
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+
+    "websocket": URLRouter([
+        re_path(r"ws/health-data/(?P<room_name>\w+)/$", HealthDataConsumer.as_asgi()),
+    ])
+})
