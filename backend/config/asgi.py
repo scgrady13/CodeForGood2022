cd4b0f5d1c34750fa9cdaf07dd31d1427c8aff50
Hoan Tran <xhoantran@gmail.com>
2022-10-22 02:53:45 -0500

    update proc

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,15 +1,13 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
-from api.consumers import HealthDataConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django_asgi_app = get_asgi_application()
 
+from api.consumers import HealthDataConsumer
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
