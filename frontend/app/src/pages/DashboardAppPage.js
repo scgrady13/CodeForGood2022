b571d8eaa2e373b3d42156a47ae3dac45e10cc7e
Preston Barnett <prestonb@tamu.edu>
2022-10-22 06:03:06 -0500

    Modify Dashboard Page QR Code

--- frontend/app/src/pages/DashboardAppPage.js
+++ frontend/app/src/pages/DashboardAppPage.js
@@ -3,7 +3,8 @@ import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography, Box, Button } from '@mui/material';
+import { Grid, Container, Typography, Box, Button, Modal } from '@mui/material';
+import QRCode from 'react-qr-code';
 // components
 import Iconify from '../components/iconify';
 // sections
@@ -23,13 +24,44 @@ import {
 // ----------------------------------------------------------------------
 
 export default function DashboardAppPage() {
+  const [open, setOpen] = useState(false);
+  const [url, setUrl] = useState("");
   const theme = useTheme();
+
+  const handleClose = () => {
+    setOpen(false);
+    setUrl(`http://google.com/${Math.floor(Math.random() * 5000)}`);
+  }
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
 
   return (
     <>
       <Helmet>
         <title> Dashboard | Minimal UI </title>
       </Helmet>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        style={{display:'flex',alignItems:'center',justifyContent:'center'}}
+      >
+        <Box sx={{ p: 5, backgroundColor: 'white', borderRadius: 5, maxWidth: 400, textAlign: 'center' }}>
+          <Typography variant="h4" sx={{ mb: 4 }}>
+            Scan the following QR code using the student's device
+          </Typography>
+          <QRCode 
+            size={256}
+            style={{ height: "200", maxWidth: "200", width: "200" }}
+            value={url}
+            viewBox={`0 0 256 256`}
+          />
+        </Box>
+      </Modal>
 
       <Container maxWidth="xl">
 
@@ -44,7 +76,7 @@ export default function DashboardAppPage() {
             </Typography>
           </Box>
           <Box>
-            <Button sx={{ ml: 3 }} variant="outlined">Add Student</Button>
+            <Button sx={{ ml: 3 }} onClick={handleOpen} variant="outlined">Add Student</Button>
           </Box>
         </Box>
 
